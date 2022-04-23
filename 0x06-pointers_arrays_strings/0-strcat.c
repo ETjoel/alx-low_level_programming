e1183cd8ffa766e376c99d30efbe9470641d84f1
ETjoel <ejoeltesfaye@gmail.com>
2022-04-23 01:52:20 -0700

    git

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int index = 0;
-        int dest_len = 0;
+	int index = 0;
+	int dest_len = 0;
 
-        while (dest[index++])
-                dest_len++;
-        for (index = 0; src[index]; index++)
-                dest[dest_len++] = src[index];
-        return (dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
